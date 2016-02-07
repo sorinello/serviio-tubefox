89742f2b88478f07871def218e56d332bf82db71
Sorin Burjan <sorinello@gmail.com>
2016-02-07 17:46:36 +0200

    - User Serviio API to push videos directly into the DLNA Server - Button changes to green checkmark if operation is completed - Uses flat chain of Promises - Closed Beta version

--- data/content-script.js
+++ data/content-script.js
@@ -6,76 +6,99 @@ self.port.on('init', function() {
 	var observer = new MutationObserver(processPage);
 	// configuration of the observer:
 	var config = {
-		attributes: true,
-		childList: true,
+		attributes   : true,
+		childList    : true,
 		characterData: true
 	};
 	// pass in the mutationNode node, as well as the observer options
 	observer.observe(document.querySelector('#content'), config)
 	console.log('Running the landing process', document.location.href)
-	// call this because refreshing OR landing directly on a Youtube page, the mutation will not trigger in time
+		// call this because refreshing OR landing directly on a Youtube page, the mutation will not trigger in time
 	processPage()
-});
 
-function processPage() {
-	currentLocation = document.location.href;
-	if (!serviioButtonExists() && currentLocation.includes('/user/') && currentLocation.includes('/videos')) {
-		var serviioURL = VIDEOS_USERNAME_BASE_URL + getChannelIdentifier()
-		createServiioVideosButton(serviioURL).prependTo('.branded-page-v2-subnav-container')
-	}
-	if (!serviioButtonExists() && currentLocation.includes('/channel/') && currentLocation.includes('/videos')) {
-		var serviioURL = VIDEOS_CHANNEL_ID_BASE_URL + getChannelIdentifier()
-		createServiioVideosButton(serviioURL).prependTo('.branded-page-v2-subnav-container')
+	function processPage() {
+		currentLocation = document.location.href;
+		if (!serviioButtonExists() && currentLocation.includes('/user/') && currentLocation.includes('/videos')) {
+			var serviioURL = VIDEOS_USERNAME_BASE_URL + getChannelIdentifier()
+			createServiioVideosButton(serviioURL).prependTo('.branded-page-v2-subnav-container')
+		}
+		if (!serviioButtonExists() && currentLocation.includes('/channel/') && currentLocation.includes('/videos')) {
+			var serviioURL = VIDEOS_CHANNEL_ID_BASE_URL + getChannelIdentifier()
+			createServiioVideosButton(serviioURL).prependTo('.branded-page-v2-subnav-container')
+		}
+
+		if (!serviioButtonExists() && currentLocation.includes('/playlist?list=')) {
+			var serviioURL = PLAYLIST_BASE_URL + getPlaylistId()
+			createServiioButton(serviioURL).appendTo('.playlist-actions')
+		}
 	}
 
-	if (!serviioButtonExists() && currentLocation.includes('/playlist?list=')) {
-		var serviioURL = PLAYLIST_BASE_URL + getPlaylistId()
-		createServiioButton(serviioURL).appendTo('.playlist-actions')
+	function serviioButtonExists() {
+		if ($('.serviioButton').length) {
+			return true
+		} else {
+			return false
+		}
 	}
-}
 
-function serviioButtonExists() {
-	if ($('.serviioButton').length) {
-		return true
-	} else {
-		return false
+	function createServiioButton(serviioURL) {
+		var serviioButton = $('<button/>', {
+			'type'  : 'button',
+			'class' : 'serviioButton yt-uix-button yt-uix-button-default',
+			'name'  : 'Serviio Button',
+		}).css({
+			'background-image': 'url(resource://serviio-foxytube/data/icon-serviio.png)',
+			'background-size' : '26px',
+			'width'           : '28px',
+			'height'          : '28px',
+		})
+
+		serviioButton.click(function() {
+			// console.log('content-script.js - copyToClipboard - Sending URL', serviioURL)
+			// console.log('Channel Title', $('.branded-page-header-title-link').text().trim())
+			// console.log('PlayList Name', $('.pl-header-title').text().trim())
+			var serviioObject = {
+				"serviioURL"  : serviioURL,
+				"channelName" : $('.branded-page-header-title-link').text().trim(),
+				"playlistName": $('.pl-header-title').text().trim()
+			}
+			self.port.emit('copyToClipboard', serviioObject)
+		})
+		return serviioButton
 	}
-}
 
-function createServiioButton(serviioURL) {
-	var serviioButton = $('<button/>', {
-		'type' : 'button',
-		'class': 'serviioButton yt-uix-button yt-uix-button-default',
-		'name' : 'Serviio Button',
-	}).css({
-		'background-image': 'url(resource://serviio-foxytube/data/icon-serviio.png)',
-		'background-size' : '26px',
-		'width'           : '28px',
-		'height'          : '28px',
+	function createServiioVideosButton(serviioURL) {
+		var serviioVideosButton = createServiioButton(serviioURL)
+		serviioVideosButton.css({
+			'float'      : 'right',
+			'margin-left': '5px'
+		})
+		return serviioVideosButton
+	}
+
+	// Returns either the user name, or the channel id.
+	function getChannelIdentifier() {
+		var tokens = document.location.href.split('/')
+		return tokens[4]
+	}
+
+	function getPlaylistId() {
+		return document.location.href.split('=')[1]
+	}
+})
+
+self.port.on('success', function() {
+	console.log('Success')
+	$('.serviioButton').css({
+		'background-image': 'url(resource://serviio-foxytube/data/success.png)',
+		'background-size' : '26px'
 	})
 
-	serviioButton.click(function() {
-		console.log('content-script.js - copyToClipboard - Sending URL', serviioURL)
-		self.port.emit('copyToClipboard', serviioURL)
-	})
-	return serviioButton
-}
+	// window.setTimeout(function(){
+	// $('.serviioButton').css({
+	// 	'background-image': 'url(resource://serviio-foxytube/data/icon-serviio.png)',
+	// 	'background-size': '26px'
+	// })
+	// }, 5000)
 
-function createServiioVideosButton(serviioURL) {
-	var serviioVideosButton = createServiioButton(serviioURL)
-	serviioVideosButton.css({
-		'float'      : 'right',
-		'margin-left': '5px'
-	})
-	return serviioVideosButton
-}
-
-// Returns either the user name, or the channel id.
-function getChannelIdentifier(){
-	var tokens = document.location.href.split('/')
-	return tokens[4]
-}
-
-function getPlaylistId() {
-	return document.location.href.split('=')[1]
-}
+})
--- index.js
+++ index.js
@@ -1,25 +1,166 @@
-var tabs      = require("sdk/tabs")
-var data      = require('sdk/self').data
+var tabs = require("sdk/tabs")
+var data = require('sdk/self').data
 var clipboard = require('sdk/clipboard')
-var pageMod   = require("sdk/page-mod")
+var pageMod = require("sdk/page-mod")
+var Request = require("sdk/request").Request;
+var preferences = require('sdk/simple-prefs').prefs;
+
 
 pageMod.PageMod({
   include: "*.youtube.com",
   contentScriptFile: [
-            data.url('jquery-2.2.0.min.js'),
-            data.url('content-script.js')],
+    data.url('jquery-2.2.0.min.js'),
+    data.url('content-script.js')
+  ],
   contentScriptWhen: 'end',
   onAttach: function(worker) {
     worker.port.emit('init')
-    worker.port.on('copyToClipboard', function(serviioURL){
-    //console.log('index.js - copyToClipboard - Received URL', serviioURL)
-    clipboard.set(serviioURL)
-    });
+    worker.port.on('copyToClipboard', function(serviioObject) {
 
-  }
-});
+      // console.log(preferences['useREST'])
+      // console.log(preferences['serviioURL'])
+
+      var serviioRESTURL = preferences['serviioURL'].replace(/\/$/, '')
+      console.log(serviioRESTURL)
+      var resolveMessages = []
+
+      function isServiioRunning() {
+        return new Promise(
+          function(resolve, reject) {
+            var serviioStatus = Request({
+              url: serviioRESTURL + '/rest/service-status',
+              contentType: 'application/json',
+              headers: {
+                accept: 'application/json'
+              },
+              onComplete: function(response) {
+                if (response.status === 200 && response.json.serviceStarted) {
+                  console.log('[isServiioRunning] - Promise fulfilled. Async code terminated')
+                  resolveMessages.push({
+                    'isServiioRunning': 'true'
+                  })
+                  resolve({
+                    'isServiioRunning': 'true'
+                  })
+                } else {
+                  console.log('[isServiioRunning] - Promise rejected. Async code terminated')
+                  reject({
+                    'isServiioRunning': 'false'
+                  })
+                }
+              }
+            }).get()
+          })
+      }
+
+      function isYoutubePluginPresent() {
+        return new Promise(
+          function(resolve, reject) {
+            var serviioStatus = Request({
+              url: serviioRESTURL + '/rest/plugins',
+              contentType: 'application/json',
+              headers: {
+                accept: 'application/json'
+              },
+              onComplete: function(response) {
+                if (response.json.plugins.length) {
+                  response.json.plugins.map(function(plugin) {
+                    if (plugin.name === 'YouTube') {
+                      console.log('[isYoutubePluginPresent] - Promise fulfilled. Async code terminated')
+                      resolveMessages.push({
+                        'isYoutubePluginPresent': 'true'
+                      })
+                      resolve({
+                        'isYoutubePluginPresent': 'true'
+                      });
+                    } else {
+                      console.log('[isYoutubePluginPresent] - Promise rejected. Async code terminated')
+                      reject({
+                        'isYoutubePluginPresent': 'false'
+                      })
+                    }
+                  })
+                } else {
+                  console.log('[isYoutubePluginPresent] - Promise rejected. Async code terminated')
+                  reject({
+                    'isYoutubePluginPresent': 'false'
+                  })
+                }
+              }
+            }).get()
+          })
+      }
+
+      function addYoutubeSource() {
+        return new Promise(
+          function(resolve, reject) {
+            var serviioStatus = Request({
+              url: serviioRESTURL + '/rest/import-export/online',
+              contentType: 'application/xml',
+              headers: {
+                accept: "application/xml"
+              },
+              content: prepareRequestBody(),
+              onComplete: function(response) {
+                if (response.status === 204) {
+                  console.log('[addYoutubeSource] - Promise fulfilled. Async code terminated')
+                  resolveMessages.push({
+                    'addYoutubeSource': 'true'
+                  })
+                  resolve({
+                    'addYoutubeSource': 'true'
+                  });
+                } else {
+                  console.log('[addYoutubeSource] - Promise rejected. Async code terminated')
+                  reject({
+                    'addYoutubeSource': 'false'
+                  })
+                }
+              }
+            }).put()
+          })
+      }
+
+      function prepareRequestBody() {
+        var serviioURL = serviioObject.serviioURL
+        var channelName = serviioObject.channelName
+        var playlistName = serviioObject.playlistName
+        var mediaSourceName
+        if (playlistName) {
+          mediaSourceName = channelName + ' - ' + playlistName
+        } else {
+          mediaSourceName = channelName
+        }
+
+        return `<onlineRepositoriesBackup>
+            <items>
+              <backupItem enabled="true">
+                <serviioLink>serviio:\/\/video:web?url=${serviioURL}&amp;name=${mediaSourceName}</serviioLink>
+                  <accessGroupIds>
+                    <id>1</id>
+                  </accessGroupIds>
+              </backupItem>
+            </items>
+          </onlineRepositoriesBackup>`
+
+      }
+
+      function informUI() {
+        console.log('resolveMessages', resolveMessages)
+        worker.port.emit('success')
+      }
+
+      isServiioRunning().then(isYoutubePluginPresent)
+        .then(addYoutubeSource)
+        .then(informUI)
+        .catch(function(err) {
+          console.log(err.fileName + ":" + err.lineNumber + ":" + err)
+        })
+    }); // end port.on
+  } // end onAttach
+}); //end PageMod
 
 tabs.open('https://www.youtube.com/user/catmusicoffice/videos')
-tabs.open('https://www.youtube.com/playlist?list=PLCzQ2UHQfewRpVrmLIEy_Dh98sEU2x0o6')
-tabs.open('https://www.youtube.com/user/erlazantivirus/videos')
-tabs.open('https://www.youtube.com/channel/UCEY2CNlzLkUedfuPAiPpEKg/videos')
+  // tabs.open('https://www.youtube.com/playlist?list=PLCzQ2UHQfewRpVrmLIEy_Dh98sEU2x0o6')
+  // tabs.open('https://www.youtube.com/user/erlazantivirus/videos')
+  // tabs.open('https://www.youtube.com/channel/UCEY2CNlzLkUedfuPAiPpEKg/videos')
